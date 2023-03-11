ce367079a05620bb2097f100e1b202122f2f76c7
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2023-03-11 15:53:34 +0200

    navbar

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
-const slider = document.querySelector(".image-slider");
-const arrLeft = document.querySelector(".arrow-left");
-const arrRight = document.querySelector(".arrow-right");
-const Heading = document.querySelector(".caption h1");
-const description = document.querySelector(".caption p");
-const preloader=document.querySelector(".loader-container");
+const slider = document.querySelector('.image-slider');
+const arrLeft = document.querySelector('.arrow-left');
+const arrRight = document.querySelector('.arrow-right');
+const Heading = document.querySelector('.caption h1');
+const description = document.querySelector('.caption p');
+const preloader = document.querySelector('.loader-container');
+const menuIcon = document.querySelector('header .icon');
+const navbar = document.querySelector('header .navbar');
 
-const images = ["1.jpg", "2.jpg", "4.jpg"];
-const heading = ["Welcome Message", "Second Message", "Another Message"];
+const images = ['1.jpg', '2.jpg', '4.jpg'];
+const heading = ['Welcome Message', 'Second Message', 'Another Message'];
 const descriptions = [
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimuspossimus voluptates",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimuspossimus voluptates? Quasi",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimuspossimus voluptates? Quasi",
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimuspossimus voluptates',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimuspossimus voluptates? Quasi',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimuspossimus voluptates? Quasi',
 ];
 
 //slider ID
 let id = 0;
 
-
 // the slider Function
 function slide(id) {
   slider.style.backgroundImage = `url(../images/${images[id]})`;
-  slider.classList.add("image-fade");
+  slider.classList.add('image-fade');
   setTimeout(() => {
-    slider.classList.remove("image-fade");
+    slider.classList.remove('image-fade');
   }, 6000);
 
   Heading.innerText = heading[id];
   description.innerText = descriptions[id];
 }
 
-// arrLeft.addEventListener("click", () => {
-//   console.log('ffa');
-//   id--;
-//   if (id < 0) {
-//     id = images.length - 1;
-//   }
-//   console.log(id);
-//   slide(id);
-// });
+arrLeft.addEventListener('click', () => {
+  id--;
+  if (id < 0) {
+    id = images.length - 1;
+  }
+  console.log(id);
+  slide(id);
+});
 
 // arrRight.addEventListener("click", () => {
 //   id++;
@@ -48,21 +48,30 @@ function slide(id) {
 // });
 
 setInterval(() => {
-    id++;
-    if (id > images.length - 1) {
-        id = 0;
-      }
-      slide(id);
-    },6000)
+  id++;
+  if (id > images.length - 1) {
+    id = 0;
+  }
+  slide(id);
+}, 6000);
 
-
-// Start preloader 
-function preloader1(){
-  preloader.classList.add('remove-loader')
+// Start preloader
+function preloader1() {
+  preloader.classList.add('remove-loader');
 }
 
-function fadeInPreLoader(){
-  setTimeout(preloader1,2000)
-} 
-window.onload=fadeInPreLoader;
-// end preloader 
+function fadeInPreLoader() {
+  setTimeout(preloader1, 2000);
+}
+window.onload = fadeInPreLoader;
+// end preloader
+
+//menu
+let isMenu = false;
+menuIcon.addEventListener('click', () => {
+  isMenu = !isMenu;
+  console.log(isMenu);
+  if (isMenu) {
+    navbar.classList.toggle('toggle-navbar');
+  }
+});
