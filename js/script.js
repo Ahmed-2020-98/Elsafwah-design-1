51f223d835ce68c1129271f4f45f68e6229dc0f5
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2023-03-18 14:36:04 +0200

    projects

--- js/script.js
+++ js/script.js
@@ -80,3 +80,123 @@ menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('toggle-navbar');
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =====================  PORTFLIO  =====================
+let allBtn = document.getElementById("all");
+let webBtn = document.getElementById("web");
+let graphicBtn = document.getElementById("graphic");
+let vectorBtn = document.getElementById("vector");
+let vectorBtn1 = document.getElementById("vector1");
+let allBtns = Array.from(document.querySelectorAll(".top .right a"));
+
+let graphicSec = document.getElementById("DesSec");
+let vectorSec = document.getElementById("VecSec");
+let vectorSec1 = document.getElementById("VecSec1");
+let webSec = document.getElementById("WebSec");
+let allSec = webSec.innerHTML + graphicSec.innerHTML + vectorSec.innerHTML + vectorSec1.innerHTML ;
+
+// all function
+allBtn.addEventListener("click", (btn) => {
+    //remove bg from all btns
+    webBtn.classList.remove("active");
+    graphicBtn.classList.remove("active");
+    vectorBtn.classList.remove("active");
+    vectorBtn1.classList.remove("active");
+    // add a bg to selected btn
+    btn.target.classList.add("active");
+    // show imgs
+    graphicSec.style.display = "block";
+    webSec.style.display = "block";
+    vectorSec.style.display = "block";
+    vectorSec1.style.display = "block";
+});
+// vector function
+vectorBtn.addEventListener("click", (btn) => {
+    //remove bg from all btns
+    allBtn.classList.remove("active");
+    webBtn.classList.remove("active");
+    graphicBtn.classList.remove("active");
+    vectorBtn1.classList.remove("active");
+    // add a bg to selected btn
+    btn.target.classList.add("active");
+    // show imgs
+    graphicSec.style.display = "none";
+    webSec.style.display = "none";
+    vectorSec.style.display = "block";
+    vectorSec1.style.display = "none";
+
+});
+// vector1 function
+vectorBtn1.addEventListener("click", (btn) => {
+    //remove bg from all btns
+    allBtn.classList.remove("active");
+    webBtn.classList.remove("active");
+    vectorBtn.classList.remove("active");
+    graphicBtn.classList.remove("active");
+    // add a bg to selected btn
+    btn.target.classList.add("active");
+    // show imgs
+    graphicSec.style.display = "none";
+    webSec.style.display = "none";
+    vectorSec1.style.display = "block";
+    vectorSec.style.display = "none";
+
+});
+// graphic function
+graphicBtn.addEventListener("click", (btn) => {
+    //remove bg from all btns
+    allBtn.classList.remove("active");
+    webBtn.classList.remove("active");
+    vectorBtn.classList.remove("active");
+    vectorBtn1.classList.remove("active");
+    // add a bg to selected btn
+    btn.target.classList.add("active");
+    // show imgs
+    graphicSec.style.display = "block";
+    webSec.style.display = "none";
+    vectorSec.style.display = "none";
+
+});
+// web function
+webBtn.addEventListener("click", (btn) => {
+    //remove bg from all btns
+    allBtn.classList.remove("active");
+    graphicBtn.classList.remove("active");
+    vectorBtn.classList.remove("active");
+    vectorBtn1.classList.remove("active");
+    // add a bg to selected btn
+    btn.target.classList.add("active");
+    // show imgs
+    graphicSec.style.display = "none";
+    webSec.style.display = "block";
+    vectorSec.style.display = "none";
+
+});
+
+
+// whats app 
+window.onscroll = () => {
+  if (this.scrollY <= 220) {
+    document.getElementById("whatsApp").style.display = "none";
+  }
+  else{
+    document.getElementById("whatsApp").style.display = "block";
+  
+  }
+}
